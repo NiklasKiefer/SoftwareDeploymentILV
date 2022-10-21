1dad1cdc01ec4ca3240b4e644accb34b827b99fa
Kiefer Niklas Johann <110473@fhwn.ac.at>
2022-10-21 14:38:19 +0200

    Simplified index.js

--- Lab2/app/index.js
+++ Lab2/app/index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('This is a NodeJS App.');
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+console.log("Hello World!");
